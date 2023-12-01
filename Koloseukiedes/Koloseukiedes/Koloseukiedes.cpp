5754c79339dd51dc32bde40efbbdf702393cb522
Mikołaj Skaziak <ms310792@student.polsl.pl>
2023-12-01 11:15:14 +0100

    --force

--- Koloseukiedes/Koloseukiedes/Koloseukiedes.cpp
+++ Koloseukiedes/Koloseukiedes/Koloseukiedes.cpp
@@ -1,51 +1,45 @@
 ﻿#include <iostream>
-#include <ctime>
-#include <cmath>
-#include <iomanip>
+
 
 using namespace std;
 
+constexpr int MAX_WIERSZE = 50;
+constexpr int MAX_KOLUMNY = 50;
+
 int main()
 {
-	setlocale(LC_ALL, "");
-	//z1
-	int x1, y1, x2, y2;
-	double d;
-	cout << "Podaj wartości (x1,y1): "; cin >> x1 >> y1;
-	cout << "Podaj wartości (x2,y2): "; cin >> x2 >> y2;
-	d = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-	cout << scientific << "Notacja naukowa: \n";
-	cout << "Odległośc eukliedesowa wynosi: " << d << endl;
-	//z2
+    setlocale(LC_ALL, "");
+    int x1, y1, x2, y2;
+    cout << "Podaj x1, y1, x2, y2: ";
+    cin >> x1 >> y1 >> x2 >> y2;
 
-	srand(time(NULL));
+    double odleglosc = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout << "Odległość między punktami: " << scientific << odleglosc << endl;
 
-	int l1 = rand() % (6);
-	char obw;
-	char** tablica = new char* [y1];
-	for (int i = 0; i < y1; i++)
-	{
-		switch (l1)
-		{
-		case 0:
-			obw = '#'; break;
-		case 1:
-			obw = '@'; break;
-		case 2:
-			obw = '$'; break;
-		case 3:
-			obw = '*'; break;
-		case 4:
-			obw = '&'; break;
-		case 5:
-			obw = '!'; break;
-		}
-		tablica[i] = new char[x1];
-		for (int j = 0; j < x1; j++)
-		{
-			l1 = rand() % (6);
-			tablica[i][j] = obw;
-			cout << tablica[i][j];
-		}
-	}
-}
+    char tablica[MAX_WIERSZE][MAX_KOLUMNY];
+    const char symbole[] = { '*', '&', '$', '#', '@', '!' };
+    srand(time(nullptr));
+
+    for (int i = 0; i < x1; ++i) {
+        for (int j = 0; j < y1; ++j) {
+            tablica[i][j] = symbole[rand() % 6];
+        }
+    }
+
+    cout << "Zawartość tablicy:" << endl;
+    for (int i = 0; i < x1; ++i) {
+        for (int j = 0; j < y1; ++j) {
+            cout << tablica[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << "Widok parzystych wierszy:" << endl;
+    for (int i = 0; i < x1; i += 2) {
+        for (int j = 0; j < y1; ++j) {
+            cout << tablica[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
